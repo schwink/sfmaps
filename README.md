73900d5cee7c4bd07454a48c85cf9d69c8353247
Stephen Schwink <stephen.schwink@gmail.com>
2025-07-14 13:46:43 -0700

    Script to crop the aligned images

--- README.md
+++ README.md
@@ -4,6 +4,17 @@ Public domain topo maps from the USGS
 
 https://www.usgs.gov/core-science-systems/ngp/topo-maps/historical-topographic-map-collection
 
+## How to use
+
 To regenerate the image files, you'll need imagemagick
 
 https://imagemagick.org
+
+`brew install imagemagick`
+
+To pull overlapping areas from the source maps, try this:
+(Coordinates are in pixels, within the 1920x1920 aligned images.)
+
+```
+./crop.py 1630 1420 270 400 mission_rock/
+```
---
+++ crop.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+
+# Crop out a matching area from each of the resized maps
+
+import argparse
+import subprocess
+import os
+
+def run_magick(args):
+    print(args)
+    subprocess.run(['magick'] + args)
+
+def crop(path_in, path_out, left, top, width, height):
+    # The images being selected from have dimensions 1920x1920
+    # left, top: Coordinates (from the top left corner) of the top left corner of the crop in pixels
+    # width, height: Dimensions of the crop in pixels
+
+    args = [
+        'convert',
+        path_in,
+        '-gravity',
+        'NorthWest',
+        '+repage',
+        '-crop',
+        '{}x{}!+{}+{}'.format(width, height, left, top),
+        '+repage',
+        path_out
+    ]
+    run_magick(args)
+
+
+IMAGES = {
+    '1895': 'CA_San Francisco_298889_1895_62500.jpg',
+    '1899': 'CA_San Francisco_298890_1899_62500.jpg',
+    '1915': 'CA_San Francisco_298896_1915_62500.jpg',
+    '1947': 'CA_San Francisco North_300059_1947_24000.jpg',
+    '1950': 'CA_San Francisco North_300060_1950_24000.jpg',
+    '1956': 'CA_San Francisco North_363065_1956_24000.jpg',
+    '1993': 'CA_San Francisco North_295005_1993_24000.jpg',
+}
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog='select',
+        description='Crop sets of map images')
+    parser.add_argument('left', type=int)
+    parser.add_argument('top', type=int)
+    parser.add_argument('width', type=int)
+    parser.add_argument('height', type=int)
+    parser.add_argument('output_dir', type=str)
+
+    args = parser.parse_args()
+
+    os.makedirs(args.output_dir, exist_ok=True)
+
+    for (year, image) in IMAGES.items():
+        input_path = image
+        output_path = '{}/{}.jpg'.format(args.output_dir, year)
+        crop(input_path, output_path, args.left, args.top, args.width, args.height)
+
+
+if __name__ == '__main__':
+    main()
